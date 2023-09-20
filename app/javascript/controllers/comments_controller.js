3572749ce42a37d25029938c0f2905344290b463
mpdacey <80190692+mpdacey@users.noreply.github.com>
2023-09-20 11:38:57 +1000

    Comment post button has variable text.

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -10,6 +10,7 @@ export default class extends Controller {
     const formID = event.params["form"];
     const form = document.getElementById(formID);
     form.classList.toggle("d-none");
+    form.classList.toggle("mt-5");
     const commentBodyID = event.params["body"];
     const commentBody = document.getElementById(commentBodyID);
     commentBody.classList.toggle("d-none");
