1b12bc538375072c72e8047180d2056b9399b0fb
mpdacey <80190692+mpdacey@users.noreply.github.com>
2023-09-22 12:17:31 +1000

    Made posts and users queryable using ransack.
    Due to recent changes to ransake, these definitions needed to be added to the models.
    
    @aleksandrawroblewska4674 posted this solutions in the youtube comments.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_noticed_notifications model_name: 'Notification'
   has_many :notifications, through: :user, dependent: :destroy
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["body", "title"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["user"]
+  end
 end
